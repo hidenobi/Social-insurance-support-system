7721080f8e86ba108dc5c60e91802580a0aee6a0
TuanVH <dungtuan095@gmail.com>
2024-03-18 17:53:34 +0700

    add create user sql

---
+++ src/main/resources/createUser.sql
@@ -0,0 +1,4 @@
+-- create database insurance;
+use insurance;
+INSERT INTO insurance.user (insurance_code, is_active, password, role, role_user) VALUES ('1234567891',1, '123456', 'USER', 'PERSONAL');
+
